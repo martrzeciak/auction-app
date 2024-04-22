0e59f6ba7ec83adee090b4a4eee63f1cdb63b152
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-04-22 22:03:40 +0200

    Add RabbitMQ

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+    {
+        public Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+        {
+            Console.WriteLine("--> Consuming faulty creation");
+
+            var exception = context.Message.Exceptions.First();
+
+            if (exception.ExceptionType == "System.ArgumentException")
+            {
+                context.Message.Message.Model = "FooBar";
+            }
+            else
+            {
+                Console.WriteLine("Not an argument exception - update error dashboard somewhere");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,13 @@ namespace AuctionService.Controllers
     {
         private readonly AuctionDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        public AuctionsController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _context = context;
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
@@ -55,6 +59,10 @@ namespace AuctionService.Controllers
 
             _context.Auctions.Add(auction);
 
+            var newAuction = _mapper.Map<AuctionDto>(auction);
+
+            await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
             var result = await _context.SaveChangesAsync() > 0;
 
             if (!result) return BadRequest("Could not save changes to the DB");
@@ -62,7 +70,7 @@ namespace AuctionService.Controllers
             return CreatedAtAction(
                 nameof(GetAuctionById),
                 new { auction.Id },
-                _mapper.Map<AuctionDto>(auction));
+                newAuction);
         }
 
         [HttpPut("{id}")]
@@ -80,6 +88,8 @@ namespace AuctionService.Controllers
             auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
             auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+            await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
             var result = await _context.SaveChangesAsync() > 0;
 
             if (result) return Ok();
@@ -96,6 +106,8 @@ namespace AuctionService.Controllers
             if (auction == null) return NotFound();
 
             _context.Auctions.Remove(auction);
+
+            await _publishEndpoint.Publish<AuctionDeleted>(new { Id = auction.Id.ToString() });
 
             var result = await _context.SaveChangesAsync() > 0;
 
