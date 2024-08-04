81c251de134bda7a16f5c12f29a2bfc38b4b3497
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-08-04 21:45:11 +0200

    Add SignalR service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,7 +14,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("notyficationPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]!);
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
